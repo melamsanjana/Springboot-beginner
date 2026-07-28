da0f2fe408445185730127e2e3494cbfc18fde34
Melam Sanjana Chakradhar <24pa1a12d7@vishnu.edu.in>
2026-07-28 23:33:01 +0530

    All the togglt,randomuser,api ,functions together

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -23,3 +23,23 @@ function toggleUser() {
     userGender.textContent=users[id].gender;
 
 }
+function randomUser() {
+    console.log("function called");
+    fetch("https://randomuser.me/api") 
+    .then(function(res) {
+        return res.json();
+    })
+    .then(function(data) {
+        var userImage=document.getElementById("user-image");
+        var userName=document.getElementById("user-name");
+        var userGender=document.getElementById("user-gender");
+
+        userImage.src=data.results[0].picture.large;
+        userName.innerHTML=data.results[0].name.first + " " + data.results[0].name.last;
+        userGender.innerHTML=data.results[0].gender;
+    })
+    .error(function(err) {
+        console.log(err);
+    })
+
+}
--- target/classes/static/index.js
+++ target/classes/static/index.js
@@ -20,6 +20,26 @@ function toggleUser() {
 
     userImage.src=users[id].image;
     userName.textContent=users[id].name;
-    userGender.textContent=users[id].gender
+    userGender.textContent=users[id].gender;
+
+}
+function randomUser() {
+    console.log("function called");
+    fetch("https://randomuser.me/api") 
+    .then(function(res) {
+        return res.json();
+    })
+    .then(function(data) {
+        var userImage=document.getElementById("user-image");
+        var userName=document.getElementById("user-name");
+        var userGender=document.getElementById("user-gender");
+
+        userImage.src=data.results[0].picture.large;
+        userName.innerHTML=data.results[0].name.first + " " + data.results[0].name.last;
+        userGender.innerHTML=data.results[0].gender;
+    })
+    .error(function(err) {
+        console.log(err);
+    })
 
 }
