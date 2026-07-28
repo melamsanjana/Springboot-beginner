2abca081a8eb4de8cf57ac279a4ec2b92ff39373
Melam Sanjana Chakradhar <24pa1a12d7@vishnu.edu.in>
2026-07-28 22:43:30 +0530

    created a toggles

--- target/classes/static/index.js
+++ target/classes/static/index.js
@@ -1,1 +1,25 @@
-alert("Hello, World!");
+var users =[
+    {
+        image: "/images/john.png",
+        name: "John Doe",
+        gender: "Male"
+    },
+    {
+        image: "/images/jane.png",
+        name: "Jane Doe",
+        gender: "Female"
+    }
+];
+var id=0;
+
+function toggleUser() {
+    id = (id + 1) % users.length;
+    var userImage=document.getElementById("user-image");
+    var userName=document.getElementById("user-name");
+    var userGender=document.getElementById("user-gender");
+
+    userImage.src=users[id].image;
+    userName.textContent=users[id].name;
+    userGender.textContent=users[id].gender
+
+}
